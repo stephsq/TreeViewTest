06d273e641eb1e354eb115b0de05c8d25d60039d
stephsq <stephsquach@gmail.com>
2026-04-08 08:52:48 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TreeViewTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            treeView = new TreeView();
+            txtBox = new TextBox();
+            btnDelete = new Button();
+            btnEdit = new Button();
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // treeView
+            // 
+            treeView.Location = new Point(12, 171);
+            treeView.Name = "treeView";
+            treeView.Size = new Size(678, 359);
+            treeView.TabIndex = 0;
+            treeView.MouseDown += treeView_MouseDown;
+            // 
+            // txtBox
+            // 
+            txtBox.Location = new Point(30, 108);
+            txtBox.Name = "txtBox";
+            txtBox.Size = new Size(638, 35);
+            txtBox.TabIndex = 8;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(486, 29);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(182, 40);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(263, 29);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(182, 40);
+            btnEdit.TabIndex = 6;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(23, 29);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(182, 40);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(702, 563);
+            Controls.Add(txtBox);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Controls.Add(treeView);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TreeView treeView;
+        private TextBox txtBox;
+        private Button btnDelete;
+        private Button btnEdit;
+        private Button btnAdd;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+namespace TreeViewTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Handle clicks in the empty space ---
+        private void treeView_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Get the node at the current mouse pointer coordinates
+            TreeNode clickedNode = treeView.GetNodeAt(e.X, e.Y);
+
+            //If there is no node under the mouse, clear the selection
+            if (clickedNode == null)
+            {
+                treeView.SelectedNode = null;
+            }
+        }
+
+        //Add a new node to the TreeView based on the text in the TextBox
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TreeNode node = new TreeNode(txtBox.Text);
+            try
+            {
+                if (treeView.SelectedNode != null)
+                {
+                    treeView.SelectedNode.Nodes.Add(node);
+                    treeView.SelectedNode.Expand();
+                }
+                else
+                {
+                    treeView.Nodes.Add(node);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding node: " + ex.Message);
+            }
+        }
+
+        //Edit the selected node's text in the TreeView
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            treeView.SelectedNode.Text = txtBox.Text;
+        }
+
+        //Delete the selected node from the TreeView
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            treeView.Nodes.Remove(treeView.SelectedNode);
+        }
+    }
+}
